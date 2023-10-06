5b64988c58d7497516bcd17a32dbc4277d12b0b7
bill <bill.arnold@student.moringaschool.com>
2023-10-06 20:09:07 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#000000";
+dodger.style.backgroundColor = "#FF69B4";
+dodger.style.bottom = "0px";
+document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft") {
+      const leftNumbers = dodger.style.left.replace("px", "");
+      const left = parseInt(leftNumbers, 10);
+  
+      dodger.style.left = `${left - 1}px`;
+    }
+  });
